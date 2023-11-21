0d61d4edfc520fdfd4586c8d6ff922ab9949968c
unknown <workuseifu@gmail.com>
2023-11-21 01:00:07 -0600

    more css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoBell, GoAlert, GoPaperclip,GoUpload,GoAlertFill } from 'react-icons/go';
+import Button from "./Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div>
+    <div>
+      <Button success rounded outline><GoBell/>Click me!</Button>
     </div>
-  );
+    <div>
+      <Button danger outline><GoAlert/>Buy Now!</Button>
+    </div>
+    <div>
+      <Button warning><GoAlertFill/>See Deal!</Button>
+    </div>
+    <div>
+      <Button secondary outline><GoPaperclip/>Hide Ads!</Button>
+    </div>
+    <div>
+      <Button primary outline><GoUpload/>Something!</Button>
+    </div>
+  </div>
 }
 
 export default App;
