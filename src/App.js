9a766f0a7371c6653acc6551eedba38dba1b7658
unknown <workuseifu@gmail.com>
2023-12-12 11:39:39 -0600

    complete button component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import { GoBell, GoAlert, GoPaperclip,GoUpload,GoAlertFill } from 'react-icons/go';
+import { GoBell, GoAlert, GoPaperclip, GoUpload, GoAlertFill } from 'react-icons/go';
 import Button from "./Button";
 
 function App() {
-  return <div>
+  const handleClick = () => {
+    console.log('clicked!');
+  };
+
+  return (
     <div>
-      <Button success rounded outline><GoBell/>Click me!</Button>
+      <div>
+        <Button
+          secondary
+          rounded
+          outline
+          className="mb-5"
+          onClick={handleClick}
+        >
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoAlert />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseLeave={handleClick}>
+          <GoAlertFill />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          <GoPaperclip />Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary outline>
+          <GoUpload />Something!
+        </Button>
+      </div>
     </div>
-    <div>
-      <Button danger outline><GoAlert/>Buy Now!</Button>
-    </div>
-    <div>
-      <Button warning><GoAlertFill/>See Deal!</Button>
-    </div>
-    <div>
-      <Button secondary outline><GoPaperclip/>Hide Ads!</Button>
-    </div>
-    <div>
-      <Button primary outline><GoUpload/>Something!</Button>
-    </div>
-  </div>
+  );
 }
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -9,26 +9,34 @@ function Button({
   warning,
   danger,
   outline,
-  rounded
+  rounded,
+  ...rest
 }) {
   const classes = twMerge(
-    className('flex items-center px-3 py-1.5 border', {
-      'border-blue-500 bg-blue-500 text-white': primary,
-      'border-gray-900 bg-gray-900 text-white': secondary,
-      'border-green-500 bg-green-500 text-white': success,
-      'border-yellow-400 bg-yellow-400 text-white': warning,
-      'border-red-500 bg-red-500 text-white': danger,
-      'rounded-full': rounded,
-      'bg-white': outline,
-      'text-blue-500': outline && primary,
-      'text-gray-900': outline && secondary,
-      'text-green-500': outline && success,
-      'text-yellow-400': outline && warning,
-      'text-red-500': outline && danger
-    })
+    className(
+      rest.className,
+      'flex items-center px-3 py-1.5 border',
+      {
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-green-500 bg-green-500 text-white': success,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger
+      })
   );
 
-  return <button className={classes}>{children}</button>
+  return (
+    <button {...rest} className={classes}>
+      {children}
+    </button>
+  );
 };
 Button.propTypes = {
   checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
